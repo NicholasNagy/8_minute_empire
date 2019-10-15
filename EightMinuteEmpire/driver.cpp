3c84c056bf32974bcb7f5ab88b67b8d2b6f8f6c8
makchamp <makramadaime@hotmail.com>
2019-10-15 18:11:18 -0400

    testing SDL gui library

--- EightMinuteEmpire/driver.cpp
+++ EightMinuteEmpire/driver.cpp
@@ -1,14 +1,43 @@
 // main driver
 
+#include "SDL.h"
+#undef main
 #include <iostream>
 using namespace std;
 
-
-/*int main()
+int main()
 
 {
-	cout << "Eight Minute Empire - By MAKRAM, AVERY, NICK and KHALED \n";
+	SDL_Window* window;                    // Declare a pointer
 
-	system("PAUSE");
+	SDL_Init(SDL_INIT_EVERYTHING);              // Initialize SDL2
+
+	// Create an application window with the following settings:
+	window = SDL_CreateWindow(
+		"Eight Minute Empire",                  // window title
+		SDL_WINDOWPOS_UNDEFINED,           // initial x position
+		SDL_WINDOWPOS_UNDEFINED,           // initial y position
+		640,                               // width, in pixels
+		480,                               // height, in pixels
+		SDL_WINDOW_OPENGL                  // flags - see below
+	);
+
+	// Check that the window was successfully created
+	if (window == NULL) {
+		// In the case that the window could not be made...
+		printf("Could not create window: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	// The window is open: could enter program loop here (see SDL_PollEvent())
+
+	SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+
+	// Close and destroy the window
+	SDL_DestroyWindow(window);
+
+	// Clean up
+	SDL_Quit();
+
 	return 0;
-}*/
+}
